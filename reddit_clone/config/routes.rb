e841019eb577a4bba26a2ccaac6016653d924ea0
SamGerber <gerbyxvi@gmail.com>
2016-01-30 14:19:51 -0800

    Add all seven subs routes

--- reddit_clone/config/routes.rb
+++ reddit_clone/config/routes.rb
@@ -9,12 +9,21 @@
 #             PATCH  /users/:id(.:format)      users#update
 #             PUT    /users/:id(.:format)      users#update
 #             DELETE /users/:id(.:format)      users#destroy
-#     session DELETE /session(.:format)        sessions#destroy
-#             POST   /session(.:format)        sessions#create
+#     session POST   /session(.:format)        sessions#create
 # new_session GET    /session/new(.:format)    sessions#new
+#             DELETE /session(.:format)        sessions#destroy
+#        subs GET    /subs(.:format)           subs#index
+#             POST   /subs(.:format)           subs#create
+#     new_sub GET    /subs/new(.:format)       subs#new
+#    edit_sub GET    /subs/:id/edit(.:format)  subs#edit
+#         sub GET    /subs/:id(.:format)       subs#show
+#             PATCH  /subs/:id(.:format)       subs#update
+#             PUT    /subs/:id(.:format)       subs#update
+#             DELETE /subs/:id(.:format)       subs#destroy
 #
 
 Rails.application.routes.draw do
   resources :users
   resource :session, only: [:new, :create, :destroy]
+  resources :subs
 end
