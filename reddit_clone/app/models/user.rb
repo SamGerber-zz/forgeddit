e332e54c8d58f3aea684bde70f9cc762065462c6
SamGerber <gerbyxvi@gmail.com>
2016-01-30 13:29:21 -0800

    Basic User Model for authentication
    Validations:
     - Must have unique username and session token
     - Must have a password_digest (persisted to db)
     - If a password is present, it must be at least 6 chars long
    
    Filters:
     - Set a token after initializing a new instance
    
     Class methods:
     ::generate_token makes a secure random url safe token
     ::find_by_credentials returns a user if the username and password match
    
     Instance methods
     #password= returns true if the password is correct
     #ensure_session_token lazily generates a token
     #reset_session_token! assigns a new session token and saves it to the DB

---
+++ reddit_clone/app/models/user.rb
@@ -0,0 +1,50 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ActiveRecord::Base
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, :session_token, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initialize :ensure_session_token_is_set
+
+  def self.generate_token
+    SecureRandom.urlsafe_base64(21)
+  end
+
+  def self.find_by_credentials(credentials)
+    user = User.find_by(username: credentials[:username])
+    user if user && user.is_password?(credentials[:password])
+  end
+
+  attr_reader :password
+
+  def password=(password)
+    @password = String(password)
+    self.password_digest = BCrypt::Password.create(@password).to_s
+    @password
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(String(password))
+  end
+
+  def ensure_session_token_is_set
+    self.session_token ||= self.class.generate_token
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_token
+    save!
+    session_token
+  end
+end
