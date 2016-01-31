b7e64fe72fc62c120c374eef3633691a947de078
SamGerber <gerbyxvi@gmail.com>
2016-01-31 08:06:06 -0800

    Annotate with schema

--- reddit_clone/app/controllers/subs_controller.rb
+++ reddit_clone/app/controllers/subs_controller.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: subs
+#
+#  id           :integer          not null, primary key
+#  title        :string           not null
+#  description  :text             not null
+#  moderator_id :integer          not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class SubsController < ApplicationController
 
   before_action :ensure_user_is_moderator, only: [:edit, :update, :destroy]
